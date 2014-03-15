d7ae8a150059698cad5c9ef2d2aa53a6c84f22ee
zaubererty <zieglert@gmx.de>
2014-03-15 03:48:52 -0700

    Added Monospace Font

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <pebble.h>
+#include <resource_ids.auto.h>
 
 struct Game {
   int state;
@@ -19,6 +20,7 @@ struct Game {
 Window *my_window;
 TextLayer *text_state_layer;
 TextLayer *text_game_time_layer;
+TextLayer *text_period_layer;
 TextLayer *text_togo_layer;
 TextLayer *text_added_time_layer;
 TextLayer *text_break_time_layer;
@@ -114,7 +116,7 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
     vibes_short_pulse();
   }
   /* Display */
-  snprintf(play_time_buffer, sizeof(play_time_buffer), "%d            %.2d:%.2d", period,  play_time / 60, play_time % 60 );
+  snprintf(play_time_buffer, sizeof(play_time_buffer), "%d %.2d:%.2d", period,  play_time / 60, play_time % 60 );
   text_layer_set_text(text_game_time_layer, play_time_buffer);
   
   snprintf(time_to_go_buffer, sizeof(time_to_go_buffer), "%.2d:%.2d", time_to_go / 60, time_to_go % 60 );
@@ -140,25 +142,28 @@ void handle_init(void) {
   Layer *window_layer = window_get_root_layer(my_window);
   /* Text Layer*/
 	text_state_layer = text_layer_create(GRect(0, 0, 144, 20));
-  text_game_time_layer = text_layer_create(GRect(0, 20, 144, 50));
+  text_game_time_layer = text_layer_create(GRect(10, 20, 144, 65));
+  text_period_layer = text_layer_create(GRect(0, 20, 144, 65));
   text_togo_layer = text_layer_create(GRect(0, 65, 72, 90));
-  text_break_time_layer = text_layer_create(GRect(72, 55, 144, 70));
-  text_added_time_layer = text_layer_create(GRect(72, 70, 144, 90));
+  text_break_time_layer = text_layer_create(GRect(72, 65, 144, 80));
+  text_added_time_layer = text_layer_create(GRect(72, 80, 144, 100));
   
-  text_layer_set_text_alignment(text_game_time_layer, GTextAlignmentRight);
+  text_layer_set_text_alignment(text_game_time_layer, GTextAlignmentCenter);
   /*text_layer_set_text_alignment(text_break_time_layer, GTextAlignmentRight);
   text_layer_set_text_alignment(text_added_time_layer, GTextAlignmentRight);*/
   
   layer_add_child(window_layer, text_layer_get_layer(text_state_layer));
   layer_add_child(window_layer, text_layer_get_layer(text_game_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(text_period_layer));
   layer_add_child(window_layer, text_layer_get_layer(text_togo_layer));
   layer_add_child(window_layer, text_layer_get_layer(text_break_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(text_added_time_layer));
   /* Font Setup */
-  text_layer_set_font(text_game_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-  text_layer_set_font(text_togo_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_font(text_game_time_layer, fonts_load_custom_font(FONT_AVERANGE_MONO_36));
+  text_layer_set_font(text_period_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_font(text_togo_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
   text_layer_set_font(text_added_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_font(text_break_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_font(text_break_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   /* Event Init */
   tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
   window_set_click_config_provider(my_window, config_provider);
